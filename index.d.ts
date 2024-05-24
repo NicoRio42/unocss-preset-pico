17e7ee74a3aafe72defbfa413c65a7eb501eeb3c
Nicolas Rio <nicolas.rio42@gmail.com>
2024-05-24 16:25:14 +0200

    first commit

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+import { Preset } from "unocss";
+
+/**
+ * Add Pico css variables to Unocss theme object, allowing you to user classes like `bg-pico-background`
+ */
+export default function presetPico(): Preset;
---
+++ index.js
@@ -0,0 +1,144 @@
+const picoCssColors = {
+  "pico-background-color": "var(--pico-background-color)",
+  "pico-color": "var(--pico-color)",
+  "pico-text-selection-color": "var(--pico-text-selection-color)",
+  "pico-muted-color": "var(--pico-muted-color)",
+  "pico-muted-border-color": "var(--pico-muted-border-color)",
+  "pico-primary": "var(--pico-primary)",
+  "pico-primary-background": "var(--pico-primary-background)",
+  "pico-primary-border": "var(--pico-primary-border)",
+  "pico-primary-underline": "var(--pico-primary-underline)",
+  "pico-primary-hover": "var(--pico-primary-hover)",
+  "pico-primary-hover-background": "var(--pico-primary-hover-background)",
+  "pico-primary-hover-border": "var(--pico-primary-hover-border)",
+  "pico-primary-hover-underline": "var(--pico-primary-hover-underline)",
+  "pico-primary-focus": "var(--pico-primary-focus)",
+  "pico-primary-inverse": "var(--pico-primary-inverse)",
+  "pico-secondary": "var(--pico-secondary)",
+  "pico-secondary-background": "var(--pico-secondary-background)",
+  "pico-secondary-border": "var(--pico-secondary-border)",
+  "pico-secondary-underline": "var(--pico-secondary-underline)",
+  "pico-secondary-hover": "var(--pico-secondary-hover)",
+  "pico-secondary-hover-background": "var(--pico-secondary-hover-background)",
+  "pico-secondary-hover-border": "var(--pico-secondary-hover-border)",
+  "pico-secondary-hover-underline": "var(--pico-secondary-hover-underline)",
+  "pico-secondary-focus": "var(--pico-secondary-focus)",
+  "pico-secondary-inverse": "var(--pico-secondary-inverse)",
+  "pico-contrast": "var(--pico-contrast)",
+  "pico-contrast-background": "var(--pico-contrast-background)",
+  "pico-contrast-border": "var(--pico-contrast-border)",
+  "pico-contrast-underline": "var(--pico-contrast-underline)",
+  "pico-contrast-hover": "var(--pico-contrast-hover)",
+  "pico-contrast-hover-background": "var(--pico-contrast-hover-background)",
+  "pico-contrast-hover-border": "var(--pico-contrast-hover-border)",
+  "pico-contrast-hover-underline": "var(--pico-contrast-hover-underline)",
+  "pico-contrast-focus": "var(--pico-contrast-focus)",
+  "pico-contrast-inverse": "var(--pico-contrast-inverse)",
+  "pico-box-shadow": "var(--pico-box-shadow)",
+  "pico-h1-color": "var(--pico-h1-color)",
+  "pico-h2-color": "var(--pico-h2-color)",
+  "pico-h3-color": "var(--pico-h3-color)",
+  "pico-h4-color": "var(--pico-h4-color)",
+  "pico-h5-color": "var(--pico-h5-color)",
+  "pico-h6-color": "var(--pico-h6-color)",
+  "pico-mark-background-color": "var(--pico-mark-background-color)",
+  "pico-mark-color": "var(--pico-mark-color)",
+  "pico-ins-color": "var(--pico-ins-color)",
+  "pico-del-color": "var(--pico-del-color)",
+  "pico-blockquote-border-color": "var(--pico-blockquote-border-color)",
+  "pico-blockquote-footer-color": "var(--pico-blockquote-footer-color)",
+  "pico-button-box-shadow": "var(--pico-button-box-shadow)",
+  "pico-button-hover-box-shadow": "var(--pico-button-hover-box-shadow)",
+  "pico-table-border-color": "var(--pico-table-border-color)",
+  "pico-table-row-stripped-background-color":
+    "var(--pico-table-row-stripped-background-color)",
+  "pico-code-background-color": "var(--pico-code-background-color)",
+  "pico-code-color": "var(--pico-code-color)",
+  "pico-code-kbd-background-color": "var(--pico-code-kbd-background-color)",
+  "pico-code-kbd-color": "var(--pico-code-kbd-color)",
+  "pico-form-element-background-color":
+    "var(--pico-form-element-background-color)",
+  "pico-form-element-selected-background-color":
+    "var(--pico-form-element-selected-background-color)",
+  "pico-form-element-border-color": "var(--pico-form-element-border-color)",
+  "pico-form-element-color": "var(--pico-form-element-color)",
+  "pico-form-element-placeholder-color":
+    "var(--pico-form-element-placeholder-color)",
+  "pico-form-element-active-background-color":
+    "var(--pico-form-element-active-background-color)",
+  "pico-form-element-active-border-color":
+    "var(--pico-form-element-active-border-color)",
+  "pico-form-element-focus-color": "var(--pico-form-element-focus-color)",
+  "pico-form-element-disabled-opacity":
+    "var(--pico-form-element-disabled-opacity)",
+  "pico-form-element-invalid-border-color":
+    "var(--pico-form-element-invalid-border-color)",
+  "pico-form-element-invalid-active-border-color":
+    "var(--pico-form-element-invalid-active-border-color)",
+  "pico-form-element-invalid-focus-color":
+    "var(--pico-form-element-invalid-focus-color)",
+  "pico-form-element-valid-border-color":
+    "var(--pico-form-element-valid-border-color)",
+  "pico-form-element-valid-active-border-color":
+    "var(--pico-form-element-valid-active-border-color)",
+  "pico-form-element-valid-focus-color":
+    "var(--pico-form-element-valid-focus-color)",
+  "pico-switch-background-color": "var(--pico-switch-background-color)",
+  "pico-switch-checked-background-color":
+    "var(--pico-switch-checked-background-color)",
+  "pico-switch-color": "var(--pico-switch-color)",
+  "pico-switch-thumb-box-shadow": "var(--pico-switch-thumb-box-shadow)",
+  "pico-range-border-color": "var(--pico-range-border-color)",
+  "pico-range-active-border-color": "var(--pico-range-active-border-color)",
+  "pico-range-thumb-border-color": "var(--pico-range-thumb-border-color)",
+  "pico-range-thumb-color": "var(--pico-range-thumb-color)",
+  "pico-range-thumb-active-color": "var(--pico-range-thumb-active-color)",
+  "pico-accordion-border-color": "var(--pico-accordion-border-color)",
+  "pico-accordion-active-summary-color":
+    "var(--pico-accordion-active-summary-color)",
+  "pico-accordion-close-summary-color":
+    "var(--pico-accordion-close-summary-color)",
+  "pico-accordion-open-summary-color":
+    "var(--pico-accordion-open-summary-color)",
+  "pico-card-background-color": "var(--pico-card-background-color)",
+  "pico-card-border-color": "var(--pico-card-border-color)",
+  "pico-card-box-shadow": "var(--pico-card-box-shadow)",
+  "pico-card-sectioning-background-color":
+    "var(--pico-card-sectioning-background-color)",
+  "pico-dropdown-background-color": "var(--pico-dropdown-background-color)",
+  "pico-dropdown-border-color": "var(--pico-dropdown-border-color)",
+  "pico-dropdown-box-shadow": "var(--pico-dropdown-box-shadow)",
+  "pico-dropdown-color": "var(--pico-dropdown-color)",
+  "pico-dropdown-hover-background-color":
+    "var(--pico-dropdown-hover-background-color)",
+  "pico-loading-spinner-opacity": "var(--pico-loading-spinner-opacity)",
+  "pico-modal-overlay-background-color":
+    "var(--pico-modal-overlay-background-color)",
+  "pico-progress-background-color": "var(--pico-progress-background-color)",
+  "pico-progress-color": "var(--pico-progress-color)",
+  "pico-tooltip-background-color": "var(--pico-tooltip-background-color)",
+  "pico-tooltip-color": "var(--pico-tooltip-color)",
+  "pico-icon-valid": "var(--pico-icon-valid)",
+  "pico-icon-invalid": "var(--pico-icon-invalid)",
+};
+
+/**
+ * @returns {import('unocss').Preset}
+ */
+function presetPico() {
+  return {
+    name: "unocss-preset-pico",
+    extendTheme: (theme) => {
+      /** @type { any} */ const newTheme = theme;
+      if (newTheme.colors !== undefined) {
+        newTheme.colors = { ...newTheme.colors, picoCssColors };
+      } else {
+        newTheme.colors = picoCssColors;
+      }
+
+      return newTheme;
+    },
+  };
+}
+
+export default presetPico;
